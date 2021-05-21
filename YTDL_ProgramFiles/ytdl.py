9d6e15d0f45ad3dcf7be5ac2b2331638780b2460
markus-weiss <markus-ws-weiss@outlook.de>
2021-05-21 16:46:03 +0200

    Stabel Downloader

--- YTDL_ProgramFiles/ytdl.py
+++ YTDL_ProgramFiles/ytdl.py
@@ -3,20 +3,19 @@ import sys
 from pytube import YouTube
 
 #try:
-#url=sys.argv[1]
+url=sys.argv[1]
 #except ValueError:
 #    print("No Input Link")
 
 targetFolder = './YOUTUBE_VIDEOS'
 
 
-url = 'https://www.youtube.com/watch?v=ZWIwLMpgcbI'
+#url = 'https://www.youtube.com/watch?v=ZWIwLMpgcbI'
 ytd = YouTube(url)
 
 print(ytd.title)
 
-ytd = YouTube(url).streams.first()
-ytd.download(targetFolder)
+ytd = YouTube(url).streams.first().download(targetFolder)
 
 
 #from os import listdir
