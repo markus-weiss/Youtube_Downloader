b5a598d46af62923162c7ab7b474ec7c73264a20
markus-weiss <markus-ws-weiss@outlook.de>
2021-05-21 14:45:19 +0200

    FolderPath Error Fix

--- YTDL_ProgramFiles/ytdl.py
+++ YTDL_ProgramFiles/ytdl.py
@@ -1,8 +1,29 @@
+import os
 import sys
 from pytube import YouTube
 
-url=sys.argv[1]
+#try:
+#url=sys.argv[1]
+#except ValueError:
+#    print("No Input Link")
 
+targetFolder = './YOUTUBE_VIDEOS'
+
+
+url = 'https://www.youtube.com/watch?v=ZWIwLMpgcbI'
 ytd = YouTube(url)
-ytd = YouTube(url).streams.first().download('/YOUTUBE_VIDEOS/')
 
+print(ytd.title)
+
+ytd = YouTube(url).streams.first()
+ytd.download(targetFolder)
+
+
+#from os import listdir
+#from os.path import isfile, join
+
+#onlyfiles = [f for f in listdir(targetFolder) if isfile(join(targetFolder, f))]
+#print(*onlyfiles)
+
+#for title in onlyfiles:
+#    print(title)
